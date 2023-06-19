b6d5ed5fed1caec800cae5ca19a002d664e32bc8
Saurabh Gupta <saurabhgupta9369899665@gmail.com>
2023-06-20 00:02:54 +0530

    .

--- Arrays/merge-overlapping-intervals.cpp
+++ Arrays/merge-overlapping-intervals.cpp
@@ -6,8 +6,7 @@ struct Interval
     int end;
     Interval() : start(0), end(0) {}
     Interval(int s, int e) : start(s), end(e) {}
-};
-class myCompare
+};class myCompare
 {
 public:
     bool operator()(Interval i1, Interval i2)
