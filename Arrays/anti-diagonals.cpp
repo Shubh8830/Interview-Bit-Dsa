8b9fecedc05ba3def8806cde646c97e6f31bc2b4
Saurabh Gupta <saurabhgupta9369899665@gmail.com>
2023-09-02 02:09:20 +0530

    /

--- Arrays/anti-diagonals.cpp
+++ Arrays/anti-diagonals.cpp
@@ -25,6 +25,8 @@ int main()
         vector<vector<int>> ans(2 * n - 1);
         for (int i = 0; i < n; i++)
         {
+
+            
             for (int j = 0; j < n; j++)
             {
                 ans[i + j].push_back(A[i][j]);
